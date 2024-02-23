204de7ae953d64ebc499316090aea20789cf3dd2
mauricioestradadlg <156852400+mauricioestradadlg@users.noreply.github.com>
2024-02-22 23:25:20 -0600

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interactivepriorityqueue;
+
+/**
+ *
+ * @author Igor Sungmin
+ */
+
+class Client {
+    private int id;
+    private String membership;
+
+    public Client(int id, String membership) {
+        this.id = id;
+        this.membership = membership;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMembership() {
+        return membership;
+    }
+}
---
+++ Product.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interactivepriorityqueue;
+
+/**
+ *
+ * @author Igor Sungmin
+ */
+class Product {
+    private String type;
+
+    public Product(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
